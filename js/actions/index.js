002c14054eda63c7c30404186bc6bfe59684241b
Ryan Bozarth <ryan_bozarth@ryan-bozarth.local>
2017-02-18 15:06:05 -0800

    added actions and reducers

--- js/actions/index.js
+++ js/actions/index.js
@@ -1,24 +1,37 @@
 // initialize game
-
 export const INIT_GAME = 'INIT_GAME';
 export const initGame = initialize => ({
-    type: 'INIT_GAME',
+    type: INIT_GAME,
     initialize
 });
 
-//generate a new random number
-
-export const GENERATE_NUMBER = 'GENERATE_NUMBER';
-export const genNumber = generate => ({
-  type: 'GENERATE_NUMBER',
-  generate
+export const TAKE_USER_NUMBER = 'TAKE_USER_NUMBER';
+export const takeUserNumber = num => ({
+  type: TAKE_USER_NUMBER,
+  num
 });
 
 // check guess value against random number
+export const COMPARE_NUMBER = 'COMPARE_NUMBER';
+export const NUMBER_MATCHED = 'NUMBER_MATCHED';
+export const COLDER = 'COLDER';
+export const HOTTER = 'HOTTER';
 
-export const VALIDATE_NUMBER = 'VALIDATE_NUMBER';
-export const compareNumber = (genNumber, currentGuess ) => ({
-  type: 'VALIDATE_NUMBER',
-  genNumber,
-  currentGuess
-});
+export const compareNumber = num => (dispatch, getState) => {
+  const {randomNumber, guesses} = getState();
+  const lastGuess = guesses[guesses.length - 1]
+  if (randomNumber === num) {
+    dispatch({
+      type: NUMBER_MATCHED
+    })
+  } else if(/*number is farther from answer than last guess*/) {
+    dispatch({
+      type: COLDER
+    })
+  } else if (/*number is farther from answer than last guess*/) {
+    dispatch({
+      type: HOTTER
+    })
+  }
+
+}
--- js/reducers/index.js
+++ js/reducers/index.js
@@ -1,16 +1,32 @@
 import * as actions from '../actions/index';
 
-const initialState = [];
+const initialState = {
+  randomNumber: Math.floor(Math.random() * 100),
+  guesses: [],
+  correctGuess: false
+};
 
 export const guessReducer = (state=initialState, action) => {
-    if (action.type === actions.INIT_GAME) {
-      return [...state, {
 
-      }];
-    } else if (action.type === GENERATE_NUMBER){
-
-    } else if (action.type === VALIDATE_NUMBER) {
-
-    }
-    return state;
+  switch (action.type) {
+    case actions.INIT_GAME:
+      return initialState
+    case actions.TAKE_USER_NUMBER:
+      return {
+        ...state,
+        guesses: [...state.guesses, action.num]
+      }
+    case actions.COMPARE_NUMBER:
+      return {
+        ...state,
+        guesses: [...state.guesses, action.num]
+      }
+    case actions.NUMBER_MATCHED:
+      return {
+        ...state,
+        correctGuess: true
+      }
+    default:
+      return state
+  }
 }
